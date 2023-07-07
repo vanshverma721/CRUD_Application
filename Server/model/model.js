45acbecd7528de31f2a56254644f8798d9dcb841
vanshverma721 <vvv.721781@gmail.com>
2023-07-07 12:31:43 +0530

    File are added

---
+++ Server/model/model.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const schema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+
+    email:{
+        type: String,
+        required: true
+    },
+
+    gender: String,
+    status: String
+})
+
+const Userdb = mongoose.model('userdb', schema);
+
+module.exports = Userdb;
